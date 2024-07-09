b35a43f2ca01842f69d8140f7edffaa48c3eaf1b
Vidhi <vidhi2005ajmera@gmail.com>
2024-07-09 20:01:37 +0530

    add user video

--- src/index.js
+++ src/index.js
@@ -11,6 +11,20 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+    app.on("error",(error)=>{
+        console.log("error: ",error);
+        throw error
+    });
+})
+.catch((err)=>{
+    console.log(
+        "MONGO DB connection failed"
+    );
+})
 
 /*
 import express from "express";
